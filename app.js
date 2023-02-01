4ff53dccb4f39e13c214fe7f96d33ff81758cea7
bokwang <chdl2353@naver.com>
2023-02-01 23:17:47 +0900

    Add api
    update, delete, get marker

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { readMarker, writeMarker } from "./db.js";
+import {
+  getMarkers,
+  createMarker,
+  updateMarker,
+  deleteMarker,
+  getMarker,
+} from "./db.js";
 
 const port = 3000;
 const app = express();
@@ -10,15 +16,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (_, res) => {
-  const markers = await readMarker();
+  const markers = await getMarkers();
   res.json(markers);
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-  await writeMarker(req.body);
-  const markers = await readMarker();
+  await createMarker(req.body);
+  const markers = await getMarkers();
   res.json(markers);
+});
+
+app.patch("/", async (req, res) => {
+  const { currentId, title, diary } = req.body;
+  await updateMarker(currentId, { title, diary });
+  res.json(await getMarkers());
+});
+
+app.delete("/", async (req, res) => {
+  const { currentId } = req.body;
+  await deleteMarker(currentId);
+  res.json(await getMarkers());
+});
+
+app.get("/marker/:id", async (req, res) => {
+  const currentId = req.params.id;
+  res.json(await getMarker(currentId));
 });
 
 app.listen(port, () => console.log(`Server is running on localhost:${port}`));
--- db.js
+++ db.js
@@ -9,13 +9,40 @@ const file = join(__dirname, "db.json");
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
-export async function readMarker() {
+export async function getMarkers() {
   await db.read();
   return db.data;
 }
 
-export async function writeMarker(newMarker) {
+export async function createMarker(newMarker) {
   await db.read();
   db.data.push(newMarker);
   await db.write();
+}
+
+export async function updateMarker(currentId, { title, diary }) {
+  await db.read();
+  db.data = db.data.map((marker) =>
+    marker.id !== currentId
+      ? marker
+      : {
+          ...marker,
+          title,
+          diary,
+        }
+  );
+  await db.write();
+}
+
+export async function deleteMarker(currentId) {
+  await db.read();
+  db.data = db.data.filter((marker) => marker.id !== currentId);
+  await db.write();
+}
+
+export async function getMarker(currentId) {
+  await db.read();
+  return db.data.find(({ id }) => {
+    id === currentId;
+  });
 }
